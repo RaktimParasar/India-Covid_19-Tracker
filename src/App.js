ddaef57d90eb1b2dc2cc554fcf166baf86eee079
RaktimParasar <raktimparasar96@gmail.com>
2021-02-13 20:53:04 +0530

    Api setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
+
 import Tracker from "./components/Tracker";
 import Map from "./components/Map";
+import { fetchData } from "./api";
 
 const App = () => {
+  useEffect(() => {
+    const data = fetchData();
+    console.log("data: ", data);
+  }, []);
+
   return (
     <div className="container">
       <Tracker />
--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,26 @@
-const url = "https://api.covid19india.org/data.json";
-const urlDaiy = "https://api.covid19india.org/states_daily.json";
+import axios from "axios";
+
+const urlData = "https://api.covid19india.org/data.json";
+const urlStates = "https://api.covid19india.org/states_daily.json";
+
+//fetch from urlData
+export const fetchData = async () => {
+  try {
+    const response = await axios.get(urlData);
+
+    return response;
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+//fetch from urlState
+// export const fetchStates = async () => {
+//   try {
+//     const response = await axios.get(urlStates);
+
+//     return response;
+//   } catch (err) {
+//     console.log(err.message);
+//   }
+// };
