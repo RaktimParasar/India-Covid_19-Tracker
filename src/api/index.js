faabdcccd9e41e454354095090a94f732904e8e2
RaktimParasar <raktimparasar96@gmail.com>
2021-02-14 09:33:14 +0530

    donut chart data

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,19 @@
 import axios from "axios";
 
 const urlData = "https://api.covid19india.org/data.json";
-const urlStates = "https://api.covid19india.org/states_daily.json";
+// const urlStates = "https://api.covid19india.org/states_daily.json";
 
 //fetch from urlData
 export const fetchData = async () => {
   try {
-    const response = await axios.get(urlData);
-
-    return response;
+    const {
+      data: { statewise },
+    } = await axios.get(urlData);
+    const totalActive = statewise[0].active;
+    const totalConfirmed = statewise[0].confirmed;
+    const totalDeceased = statewise[0].deaths;
+    const totalRecovered = statewise[0].recovered;
+    return { totalActive, totalConfirmed, totalDeceased, totalRecovered };
   } catch (err) {
     console.log(err.message);
   }
@@ -17,7 +22,7 @@ export const fetchData = async () => {
 //fetch from urlState
 // export const fetchStates = async () => {
 //   try {
-//     const response = await axios.get(urlStates);
+//     const response = await axios.get(urlData);
 
 //     return response;
 //   } catch (err) {
